7b4ca2a54b7568b210c28411a366898c91d66bbc
Louise Crow <louise.crow@gmail.com>
2014-06-28 15:40:32 +0100

    Add blog posts and featured requests.

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -5,5 +5,25 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
+    GeneralController.class_eval do
+            # Make sure it doesn't break if blog is not available
+        def frontpage
+            begin
+                blog
+            rescue
+                puts "ERROR"
+                @blog_items = []
+                @twitter_user = MySociety::Config.get('TWITTER_USERNAME', '')
+            end
 
+            begin
+                @featured_requests = InfoRequest.find( :all,
+                                                        :conditions => ["prominence='normal'"],
+                                                        :order => "updated_at desc",
+                                                        :limit => 3)
+            rescue
+                @featured_requests = []
+            end
+        end
+    end
 end
